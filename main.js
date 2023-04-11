80846609006e09ed0104cd30ec62357a601e5cbf
Aditya Kumar <121349409+adityakumar1108@users.noreply.github.com>
2023-04-11 13:24:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+//Select element function
+
+const selectElement = function(element){
+	return document.querySelector(element);
+};
+
+let menuToggler = selectElement('.menu-toggle');
+let body = selectElement('body');
+
+menuToggler.addEventListener('click', function(){
+	body.classList.toggle('open');
+});
+
+//Scrool reveal
+
+window.Sr = ScrollReveal();
+
+Sr.reveal('.animate-left',{
+	origin: 'left',
+	duration: 1000,
+	distance: '25rem',
+	delay: 300
+});
+
+Sr.reveal('.animate-right',{
+	origin: 'right',
+	duration: 1000,
+	distance: '25rem',
+	delay: 600
+});
+
+Sr.reveal('.animate-top',{
+	origin: 'top',
+	duration: 1000,
+	distance: '25rem',
+	delay: 600
+});
+
+Sr.reveal('.animate-bottom',{
+	origin: 'bottom',
+	duration: 1000,
+	distance: '25rem',
+	delay: 600
+});
